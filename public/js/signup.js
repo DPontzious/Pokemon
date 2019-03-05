d15c0711b395c3509459191f5ba51a46d6d9ef22
kayaktavist <imagerybyar@gmail.com>
2019-03-05 12:47:28 -0700

    3/5 5th

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,23 +5,26 @@ $(document).ready(function() {
     var passwordInput = $("input#password-input");
   
     // When the signup button is clicked, we validate the email and password are not blank
-    signUpForm.on("submit", function(event) {
+    $("#signup").on("click", function(event) {
       event.preventDefault();
-      console.log("signup onclick")
       var userData = {
         email: emailInput.val().trim(),
         password: passwordInput.val().trim()
       };
-  
+      
       if (!userData.email || !userData.password) {
         return;
+      }
+      else {
+        console.log("Input error")
       }
       // If we have an email and password, run the signUpUser function
       signUpUser(userData.email, userData.password);
       emailInput.val("");
       passwordInput.val("");
+      console.log("signup onclick")
     });
-  
+    
     // Does a post to the signup route. If successful, we are redirected to the members page
     // Otherwise we log any errors
     function signUpUser(email, password) {
