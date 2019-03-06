dd6b8945fbbca237c28c57b7815d8bd45dbd7f1f
Danelle Pontzious <danellepontzious@Danelles-MacBook-Air.local>
2019-03-06 10:14:29 -0700

    moved files

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,6 @@
 var db = require("../models");
 var passport = require("../config/passport");
 var question = require("../data/questionsinfo");
-
 module.exports = function (app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
