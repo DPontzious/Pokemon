df3fbffbbb69bdbdd3f4febb8c9224ab30a65e77
kayaktavist <imagerybyar@gmail.com>
2019-03-06 12:02:22 -0700

    3/6 5th

--- routes/data/questionsinfo.js
+++
@@ -1,5 +0,0 @@
-var questionsinfo = {
-    name: "testing",
-    scores: ['3', '4', '2', '3', '3', '5', '5', '4', '5', '5']
-}
-module.exports = questionsinfo;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,10 +2,10 @@ module.exports = function (app) {
     app.get("/", (req, res) => {
         res.render("index");
     });
-    app.get("/questions", (req, res) => {
-        // res.sendFile(path.join(__dirname, "../views/questions"));
-        res.render("questions");
-    });
+    // app.get("/questions", (req, res) => {
+    //     // res.sendFile(path.join(__dirname, "../views/questions"));
+    //     res.render("questions");
+    // });
     
     // Character status and interaction page route
     app.get("/status", (req, res) => {
